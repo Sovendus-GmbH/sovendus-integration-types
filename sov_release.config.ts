b2d7965595cd87f1217d4c93e7948bf46ed0c4b8
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-02-26 20:46:08 +0100

    chore: update TypeScript configuration and add release tool script

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -1,46 +1,18 @@
-import type { ReleaseConfig } from "./release-tool/types";
+import type { ReleaseConfig } from "sovendus-release-tool";
 
 const releaseConfig: ReleaseConfig = {
   packages: [
     {
       directory: "./",
-      release: false,
       updateDeps: true,
-      version: "1.0.0",
-    },
-
-    {
-      directory: "integration-types",
-      updateDeps: true,
+      version: "3.3.21",
       release: true,
       releaseOptions: {
-        tagPrefix: "types_",
         foldersToScanAndBumpThisPackage: [
-          { folder: "integration-settings-ui" },
-          { folder: "integration-scripts" },
+          // scan whole dev env
+          { folder: "../../../../" },
         ],
       },
-      version: "3.3.17",
-    },
-    {
-      directory: "integration-settings-ui",
-      updateDeps: true,
-      release: true,
-      releaseOptions: {
-        tagPrefix: "settings_ui_",
-        foldersToScanAndBumpThisPackage: [],
-      },
-      version: "3.4.2",
-    },
-    {
-      directory: "integration-scripts",
-      updateDeps: true,
-      release: true,
-      releaseOptions: {
-        tagPrefix: "scripts_",
-        foldersToScanAndBumpThisPackage: [],
-      },
-      version: "3.3.21",
     },
   ],
 };
