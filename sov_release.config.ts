874fc590b3663cf452a21d446179a0c494542d96
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-02-26 20:46:19 +0100

    Release: 3.3.22

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -5,7 +5,7 @@ const releaseConfig: ReleaseConfig = {
     {
       directory: "./",
       updateDeps: true,
-      version: "3.3.21",
+      version: "3.3.22",
       release: true,
       releaseOptions: {
         foldersToScanAndBumpThisPackage: [
