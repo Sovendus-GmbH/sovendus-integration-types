2136ea65c67bf42c9a43ad584ea31ccefc78fc83
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-07 10:50:06 +0100

    feat: add optional iframeContainerId to SovendusThankYouPageConfig

--- src/v3/plugin-thankyou.ts
+++ src/v3/plugin-thankyou.ts
@@ -14,6 +14,7 @@ export const defaultSovendusThankyouPageConfig: SovendusThankYouPageConfig = {
   settings: defaultSovendusAppSettings,
   integrationType: "",
   sovDebugLevel: undefined,
+  iframeContainerId: undefined,
   orderData: {},
   customerData: {},
 } as const;
@@ -59,6 +60,7 @@ export interface SovendusThankyouWindow
 export interface SovendusThankYouPageConfig {
   settings: SovendusAppSettings;
   integrationType: string;
+  iframeContainerId?: string | undefined;
   sovDebugLevel: SovDebugLevel | undefined;
   orderData: SovendusConversionsData;
   customerData: SovendusConsumerData;
