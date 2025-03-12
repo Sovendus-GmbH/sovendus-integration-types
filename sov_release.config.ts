39f71cd0e791a85614c307a6fccccd4b2ea2c175
daniel.rehberg@sovendus.com <daniel.rehberg@sovendus.com>
2025-03-12 16:10:29 +0100

    Release: 3.3.27

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -5,7 +5,7 @@ const releaseConfig: ReleaseConfig = {
     {
       directory: "./",
       updateDeps: true,
-      version: "3.3.26",
+      version: "3.3.27",
       release: true,
       lintAndBuild: true,
       test: false, // TODO add some zod validation test
