c39bcff4876448c5ce6ddbd437b592721296960b
Marcus Brandstaetter <marcus.brandstaetter@sovendus.com>
2025-03-19 16:30:37 +0100

    Release: 3.7.1

--- sov_release.config.ts
+++ sov_release.config.ts
@@ -9,7 +9,7 @@ const releaseConfig: ReleaseConfig = {
       build: true,
       test: false, // TODO add some zod validation test
       release: {
-        version: "3.7.0",
+        version: "3.7.1",
         foldersToScanAndBumpThisPackage: [
           // scan whole dev env
           { folder: "../../../../" },
